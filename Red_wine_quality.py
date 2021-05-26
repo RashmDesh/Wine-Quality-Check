df48f0029caea98187a2904a4cca062acbbd88d7
Rashmi Deshmukh <rashdeshmukh93@gmail.com>
2021-05-26 20:04:10 +0530

    Update Red_wine_quality.py

--- Red_wine_quality.py
+++ Red_wine_quality.py
@@ -19,22 +19,25 @@ def run():
 
     fixed_acidity=st.slider("1.Select fixied acidity level :",4.0,16.0)
     volatile_acidity=st.number_input("2.Select volatile acidity level(Min=0.00 and Max=2.00) :")
+    citric_acid=st.slider("4.Select citric acid level :",0.0,1.0)
     residual_sugar=st.slider("3.Select residual sugar level:",0,15)
+    density=st.slider("4.Select density level :",0.0,1.0)
+    sulphates=st.slider("4.Select sulphates level :",0.0,2.0)
     pH=st.slider("4.Select pH level :",2.0,4.0)
     alcohol=st.slider("5.Select alcohol level :",1,14)
 
     st.markdown(html_temp, unsafe_allow_html=True)
 
     if st.button("Submit"):
-        features = [[fixed_acidity,volatile_acidity,residual_sugar,pH,alcohol]]
+        features = [[fixed_acidity,volatile_acidity,citric_acid,residual_sugar,density,pH,sulphates,alcohol]]
         #st.table(features)
         prediction = model.predict(features)
         lc = [str(i) for i in prediction]
         ans = int("".join(lc))
         st.write("Quality of wine is ",ans," out of 10")
-        if ans >=6:
+        if ans >6:
             st.success("Quality of Wine is Superb")
-        elif(ans<6 and ans >= 5):
+        elif(ans<=6 and ans >= 5):
             st.success("Quality of Wine is Good")
         else:
             st.success("Quality of  Wine  is Bad")
